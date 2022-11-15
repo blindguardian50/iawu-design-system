2be909e3afd988d6355c7d4887aecdcd25e8c8bc
David Egger <david.egga@gmx.at>
2022-11-15 12:12:39 +0100

    Fix some Alert bugs, Add args table documentation for Alert

--- src/components/Alerts/Alerts.tsx
+++ src/components/Alerts/Alerts.tsx
@@ -13,19 +13,31 @@ const BaseAlertWrapper = styled(TemplateAlert)`
 `
 
 type BaseAlertStyleProps = {
-  colorCSSVar?: string,
+  /**
+   * Expects CSS Variable for Background RGB values in format of: <R>, <G>, <B>
+   */
   backgroundCSSVarRGB?: string,
+  /**
+   * Expects CSS Variable for font color. Defaults to hold the same color value as backgroundCSSVarRGB.
+   */
+  colorCSSVar?: string,
+  /**
+   * Expects CSS Variable for font of Alert. If no value is given the font will be inherited from parent.
+   */
   fontCSSVar?: string
+  /**
+   * Expects Opacity value of background from 0 to 1.
+   */
   opacity?: string
 }
 
 const BaseAlertStyled = styled(TemplateAlert)<BaseAlertStyleProps>`
   display: flex;
-  color: ${props => props.backgroundCSSVarRGB ? `rgb(var(${props.backgroundCSSVarRGB}))` :
-          props.colorCSSVar ? `var(${props.backgroundCSSVarRGB})` : 
-                  `var("--light-color")`};
+  color: ${props => props.colorCSSVar ? `var(${props.colorCSSVar})` : 
+      props.backgroundCSSVarRGB ? `rgb(var(${props.backgroundCSSVarRGB}))` : 
+          `var("--light-color")`};
   background: rgba(var(${props => props.backgroundCSSVarRGB ?? "--brand-color-rgb"}), ${props => props.opacity ?? 0.4});
-  ${props => props.fontCSSVar ? `font: var(${props.fontCSSVar};` : ""}
+  ${props => props.fontCSSVar ? `font: var(${props.fontCSSVar});` : ""}
   border: 1px solid rgb(var(${props => props.backgroundCSSVarRGB ?? "--brand-color-rgb"}));
 `
 
